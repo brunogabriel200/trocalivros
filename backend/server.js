855e6584329188f5c3100fa48f95b13a2ffb8f3a
Bruno Gabriel <168151070+brunogabriel200@users.noreply.github.com>
2025-07-21 19:05:48 -0300

    Update 10 server.js

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,8 @@ const connection = require('./db');
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json({ limit: '10mb' }));
-app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+app.use(bodyParser.json({ limit: '20mb' }));
+app.use(bodyParser.urlencoded({ limit: '20mb', extended: true }));
 
 
 app.get('/', (req, res) => {
