131a9ea6619095e189dafcaedb588fa3cc223153
Bruno Gabriel <168151070+brunogabriel200@users.noreply.github.com>
2025-07-31 22:47:08 -0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
   password: '@Trocalivros',
-  database: 'trocalivros',
+  database: 'troca_livros',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
