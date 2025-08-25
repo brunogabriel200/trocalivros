03c84b7995f0e7988013e2bb48f91a7a3d9c2230
Bruno Gabriel <168151070+brunogabriel200@users.noreply.github.com>
2025-08-25 18:22:16 -0300

    Add files via upload

---
+++ scripts/cadastro.js
@@ -0,0 +1,27 @@
+document.querySelector('form').addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const nome = document.querySelector('#name').value;
+  const email = document.querySelector('#email').value;
+  const senha = document.querySelector('#password').value;
+
+  try {
+    const resposta = await fetch('https://trocalivros.duckdns.org/cadastro', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ nome, email, senha })
+    });
+
+    const dados = await resposta.json();
+
+    if (resposta.ok) {
+      alert('Cadastro realizado com sucesso!');
+      window.location.href = '../index.html';
+    } else {
+      alert(dados.erro);
+    }
+  } catch (erro) {
+    alert('Erro ao cadastrar');
+    console.error(erro);
+  }
+});
